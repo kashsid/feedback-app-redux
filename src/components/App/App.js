ea2226de8dda69555dab984d680a4c8ba39aa1a7
kashsid <kashifsiddique@gmail.com>
2019-04-06 17:49:24 -0500

    implements routes in Aop. Implements reducer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,47 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+// import and destructure router components from react-redux
+import { HashRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+// Import Route components:
+import Feeling from "../Feeling/Feeling";
+import Understanding from "../Understanding/Understanding";
+import Support from "../Support/Support";
+import Comments from "../Comments/Comments";
+import ReviewFeedback from "../ReviewFeedback/ReviewFeedback";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4>
+              <i>Don't forget it!</i>
+            </h4>
+          </header>
+          <br />
+          <div>
+            {/* routes will go here... */}
+            <Route exact path="/" component={Feeling} />
+            <Route exact path="/understanding" component={Understanding} />
+            <Route exact path="/support" component={Support} />
+            <Route exact path="/comments" component={Comments} />
+            <Route
+              exactpath="/review"
+              render={props => (
+                <ReviewFeedback
+                  {...props}
+                  submitFeedback={this.submitFeedback}
+                />
+              )}
+            />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
-
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from "./registerServiceWorker";
+import logger from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+
+const feedback = {
+  feeling: "",
+  understanding: "",
+  support: "",
+  comments: ""
+};
+
+const feedbackReducer = (state = feedback, action) => {
+    switch (action.type) {
+        case 'SET_FEEDBACK_FEELING':
+            //console.log('state is ',state);
+            return { ...state, feeling: action.payload }
+
+        case 'SET_FEEDBACK_UNDERSTANDING':
+            return { ...state, understanding: action.payload }
+        case 'SET_FEEDBACK_SUPPORT':
+            return { ...state, support: action.payload }
+        case 'SET_FEEDBACK_COMMENTS':
+            return { ...state, comments: action.payload }
+        default:
+            break;
+    }
+    return state;
+
+
+
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        // reducers....
+        feedbackReducer,
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(
+    <Provider store={storeInstance} >
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
