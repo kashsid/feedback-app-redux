20cbcdff1aebb06a46b5ff34c283af9bd88760a6
kashsid <kashifsiddique@gmail.com>
2019-04-06 18:35:40 -0500

    implements undesranding component

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux";
+import ReviewFeedback from "../ReviewFeedback/ReviewFeedback";
 
 export class Understanding extends Component {
+    state = {
+        understanding: 0
+    };
+
+    handleChange = (event) => {
+        console.log(`in handleChange...`, this.state);
+
+        this.setState({
+            ...this.state,
+            feeling: event.target.value,
+        })
+    }
+
+    handleSubmit = () => {
+        console.log(`in handleSubmit...`);
+
+
+        if (this.state.feeling !== '') {
+            const action = {
+                type: "SET_FEEDBACK_UNDERSTANDING",
+                payload: this.state.feeling
+            };
+            this.props.dispatch(action);
+
+            this.props.history.push('/support');
+        } else {
+            alert(`Please select a number between 1 and 5.`);
+        }
+    }
   render() {
     return (
       <div>
-            <h1> How well are you understanding the content?</h1>
-      </div>
-    )
-  }
+            <h2> How well are you understanding the content?</h2>
+                <form onSubmit={this.handleSubmit} >
+                    <input type="number" name="support"
+                        min="1" max="5"
+                        onChange={this.handleChange} />
+
+                    <button type="submit">Next</button>
+                </form>
+
+                <br />
+                <ReviewFeedback />
+            </div>
+        );
+    }
 }
 
-export default Understanding
+export default connect()(Understanding);
+
